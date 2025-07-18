a272863bb3075fbea1835986be0393201367818d
Giorgi G. <76514960+giorgi-py@users.noreply.github.com>
2025-07-18 16:40:08 +0400

    Update wsgi.py

--- bitworks/wsgi.py
+++ bitworks/wsgi.py
@@ -1,34 +1,5 @@
-"""
-WSGI config for bitworks project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bitworks.settings')
-
-# application = get_wsgi_application()
-
-
 import os
-        import sys
+from django.core.wsgi import get_wsgi_application
 
-        # Add your project directory to the Python path
-        sys.path.insert(0, os.path.dirname(__file__))
-
-        # Set the DJANGO_SETTINGS_MODULE environment variable
-        os.environ['DJANGO_SETTINGS_MODULE'] = 'your_project_name.settings' # Replace 'your_project_name'
-
-        # Initialize Django
-        import django
-        django.setup()
-
-        # Import the WSGI application
-        from django.core.wsgi import get_wsgi_application
-        application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bitworks.settings')
+application = get_wsgi_application()
