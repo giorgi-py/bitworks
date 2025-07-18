6d98f8a3aa648977e6e924701847a4fef8306240
Giorgi G. <76514960+giorgi-py@users.noreply.github.com>
2025-07-18 17:03:10 +0400

    Update settings.py

--- bitworks/settings.py
+++ bitworks/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 LOCALE_PATHS = [BASE_DIR / 'locale']
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # <--- This line is important!
 ]
