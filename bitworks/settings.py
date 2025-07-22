7235b37f1c9761f3b24b56b19bd0ae4c0076f4f0
Giorgi G. <76514960+giorgi-py@users.noreply.github.com>
2025-07-22 17:46:10 +0400

    Update settings.py
    Added ALLOWED_HOSTS only to bitworks.ge and www.bitworks.ge

--- bitworks/settings.py
+++ bitworks/settings.py
@@ -29,7 +29,7 @@ DEBUG = env('DEBUG')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['bitworks.ge', 'www.bitworks.ge']
 CSRF_TRUSTED_ORIGINS = [
     'http://bitworks.ge',
     'http://www.bitworks.ge',
